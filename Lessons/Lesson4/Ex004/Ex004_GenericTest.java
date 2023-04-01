cbe4dea4850835717b739a4317eff93882278928
Profi <faris-d@mail.ru>
2023-04-01 22:35:39 +0300

    Подправил урок 4

---
+++ Lessons/Lesson4/Ex004/Ex004_GenericTest.java
@@ -0,0 +1,9 @@
+package Lessons.Lesson4.Ex004;
+public class Ex004_GenericTest {
+    public static void main(String[] args) {
+        // T type
+        // E element
+        // K key
+        // V value
+    }
+}
